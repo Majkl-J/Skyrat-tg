541e2d45b4cab13b1abac26cbcc6c926296867bf
projectkepler-RU <99981766+projectkepler-ru@users.noreply.github.com>
2024-09-08 21:00:45 +0700

    hat

--- tgui/packages/tgui/interfaces/VotePanel.tsx
+++ tgui/packages/tgui/interfaces/VotePanel.tsx
@@ -278,12 +278,8 @@ const ChoicesPanel = (props) => {
                         name="vote-yea"
                       />
                     )}
-<<<<<<< HEAD
                   {currentVote.displayStatistics ||
                   user.isLowerAdmin /* SKYRAT EDIT*/
-=======
-                  {currentVote.displayStatistics
->>>>>>> 4b4ae0958fe6b5d511ee6e24a5087599f61d70a3
                     ? choice.votes + ' Votes'
                     : null}
                 </LabeledList.Item>
